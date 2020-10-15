3165dd64c7fee1a1137068bc5e7cb0fd60673bb0
Andrey Mityagov <amityagov@gmail.com>
2020-10-15 23:14:08 +0300

    arity sql folder attribute

---
+++ DotnetMigrations.Arity.DataAnnotations/SqlAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Arity;
+
+namespace DotnetMigrations.Arity.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SqlFolderAttribute : ModuleMetadataAttribute
+    {
+        public string Path { get; }
+
+        public SqlFolderAttribute(string path)
+        {
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        public override void Apply(ModuleMetadata metadata)
+        {
+            metadata.Items[ModuleMetadataKeys.PathKey] = Path;
+        }
+    }
+}
